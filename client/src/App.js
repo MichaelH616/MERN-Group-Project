bc9d64ccc0e5f620cf88d5cb5e8f2ff4b2d36235
MichaelH616 <michaelhuang6160@gmail.com>
2023-04-18 19:48:03 -0700

    project setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
