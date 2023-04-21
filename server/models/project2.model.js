c1c02b7ec4c5d22e745fa5ed257926d81a49144c
MichaelH616 <michaelhuang6160@gmail.com>
2023-04-21 13:49:35 -0700

    Michael's updates

---
+++ server/models/project2.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const taskModel = require('./task.model');
+const userModel = require('./user.model')
+
+const ProjectSchema = new mongoose.Schema({
+    projectName: {
+        type: String,
+        required: [true, "Project name is required"],
+        min: [2, "Project name must be at least 2 characters"]
+    },
+    description: {
+        type: String,
+        required: [true, "Project description is required"]
+    },
+    dueDate: {
+        type: Date,
+        required:[true, "Date is required"]
+
+    },
+    completedStatus: {
+        type: Boolean,
+        required: [true, "Is the project completed?"]
+    },
+    tasks: [taskModel],
+    users: [userModel]
+})
+
+module.exports = mongoose.model("Project", ProjectSchema);
