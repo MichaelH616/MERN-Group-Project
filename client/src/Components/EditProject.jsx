44433c9dc38de92349263567b65de74bc2a20ebd
MichaelH616 <michaelhuang6160@gmail.com>
2023-04-21 15:26:55 -0700

    added edit page

--- client/src/Components/EditProject.jsx
+++ client/src/Components/EditProject.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const AddProject = (props) => {
+const EditProject = (props) => {
     const {projectList, setProjectList} = props 
     const [title, setTitle] = useState ("");
     const [tasks, setTasks] = useState ("");
@@ -9,68 +10,65 @@ const AddProject = (props) => {
     const [dueDate, setDueDate] = useState ("");
     const [member, setMember] = useState ("");
 
+    const { id } = useParams();
+
     const handleSubmit = (e)=>{
         e.preventDefault();
 
-    axios.post("http://localhost:8000/api/project",{title, tasks, description, dueDate, member}, {withCredentials:true})
-        .then((res)=>{
-            console.log(res);
-            console.log(res.data);
-            setProjectList([...projectList, res.data]);
-            setTitle("");
-            setTasks("");
-            setDescription("");
-            setDueDate("");
-            setMember("");
+    useEffect(() => {
+        axios.get((`/api/project/${id}`), {withCredentials:true})
+            .then(res => {
+                setProjectList(res.data);
             })
-        .catch((err)=>{
-            console.log(err);
-            });
-    }
+            .catch(err => {
+                console.log(err);
+            })
+    })
 
     return ( 
         <div>
-            <h1>Add a Project</h1>
+            <h1>Edit a Project</h1>
             <form action="" className="form col-md-4 mx-auto" onSubmit={handleSubmit}>
                 <div className='form-group mt-3'>
                     {
                     title && title.length < 3 ? <p className='text-danger'>Title must be at least 2 characters</p>:""}
                     <label htmlFor="" className='form-label'>Title: </label>
-                    <input type="text" name='title' className="form-control" onChange={(e)=>{setTitle(e.target.value)}}/>
+                    <input type="text" name='title' value={title} className="form-control" onChange={(e)=>{setTitle(e.target.value)}}/>
                 </div>
                 <div className='form-group mt-3'>
                     {
                     tasks && tasks.length < 3 ? <p className='text-danger'>Tasks must be at least 2 characters.</p>:""}
                     <label htmlFor="" className='form-label'>Tasks : </label>
-                    <input type="number" name="tasks" className="form-control" onChange={(e)=>{setTasks(e.target.value)}}  />
+                    <input type="number" name="tasks" value={tasks} className="form-control" onChange={(e)=>{setTasks(e.target.value)}}  />
                 </div>
                 <div className='form-group mt-3'>
                     {
                     description && description.length < 5 ? <p className='text-danger'>Description must be at least 5 characters</p>:""   
                     }
                     <label htmlFor='' className='form-label'>Description: </label>
-                    <input type="text" name="description" id=""className='form-control' onChange={(e)=>{setDescription(e.target.value)}} />
+                    <input type="text" name="description" value={description}id=""className='form-control' onChange={(e)=>{setDescription(e.target.value)}} />
                 </div>
                 <div className='form-group mt-3'>
                     {
                     dueDate ? <p className='text-danger'>Due Date is required.</p>:""
                     }
                     <label htmlFor='' className='form-label'> Due Date: </label>
-                    <input type="date" name="dueDate" id=""className='form-control' onChange={(e)=>{setDueDate(e.target.value)}} />
+                    <input type="date" name="dueDate" id="" value={date} className='form-control' onChange={(e)=>{setDueDate(e.target.value)}} />
                 </div>
                 <div className='form-group mt-3'>
                     {
                     member && member.length < 2 ? <p className='text-danger'>Member is required.</p>:""
                     }
                     <label htmlFor='' className='form-label'>Member(s): </label>
-                    <input type="text" name="member" id=""className='form-control' onChange={(e)=>{setMember(e.target.value)}} />
+                    <input type="text" name="member" id="" value={member} className='form-control' onChange={(e)=>{setMember(e.target.value)}} />
                 </div>
                 <br>
                 </br>
-                <input className="submit-input" type="submit" value="Create Project" />
+                <input className="submit-input" type="submit" value="Update Project" />
             </form>
         </div>
     )
 }
 
-export default AddProject            
+}
+export default EditProject            
--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,10 +1,11 @@
 const ProjectController = require("../controllers/project.controller");
+const { authenticate } = require('../config/jwt.config');
 
 module.exports = (app) => { 
-    app.post('/api/project', ProjectController.createNewProject);
-    app.get('/api/projects', ProjectController.findAllProjects);
-    app.get('/api/project/:id', ProjectController.findOneProject);
-    app.get('/api/projected/:member', ProjectController.findByMember);
-    app.delete('/api/projects/:id', ProjectController.deleteProject);
-    app.put('/api/projects/:id', ProjectController.updateProject);
+    app.post('/api/project', authenticate, ProjectController.createNewProject);
+    app.get('/api/projects', authenticate, ProjectController.findAllProjects);
+    app.get('/api/project/:id', authenticate, ProjectController.findOneProject);
+    app.get('/api/projected/:member', authenticate, ProjectController.findByMember);
+    app.delete('/api/projects/:id', authenticate, ProjectController.deleteProject);
+    app.put('/api/projects/:id', authenticate, ProjectController.updateProject);
 }
